0aa95474bb51c395d4272334e778f2c24fe7eae7
7exodus <alphacharlie.2077@gmail.com>
2025-03-14 23:56:24 +0100

    upload
    dd

---
+++ PidGet/PidGet/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "sdk.hpp"
+
+int main()
+{
+	subx123();
+	return 0;
+}
---
+++ PidGet/PidGet/sdk.hpp
@@ -0,0 +1,12 @@
+void subx123()
+{
+	system("color 1");
+
+	std::cout << "7exodus\n";
+
+	system("timeout /t 3");
+	system("cls");
+	system("color 1");
+	system("tasklist");
+	system("pause");
+}
