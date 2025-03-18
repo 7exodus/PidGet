d027f6e5abb6e63dd51dc539cc250f25dde8d15d
7exodus <alphacharlie.2077@gmail.com>
2025-03-18 16:00:44 +0100

    the code structure has been improved

---
+++ PidGet/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//sdk.hpp
+#include "sdk.hpp"
+
+int main()
+{
+	subx123();
+	return 0;
+}
---
+++ PidGet/sdk.hpp
@@ -0,0 +1,11 @@
+void subx123()
+{
+	system("color 1");
+	std::cout << "7exodus\n";
+
+	system("timeout /t 3");
+	system("cls");
+
+	system("tasklist");
+	system("pause");
+}
